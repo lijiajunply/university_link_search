13e2c0a7d5266377631ba929676f21df9599e31c
李嘉俊 <2383690354@qq.com>
2025-06-08 17:31:27 +0800

    加入一些响应式操作

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="mx-auto px-16 pb-4 pt-8">
+  <div class="mx-auto px-8 md:px-16 pb-4 pt-8">
     <footer class="bg-white/20 backdrop-blur-sm rounded-2xl  text-center p-8">
       <p class="layout-footer-title mb-4">
         &copy; 2025 iOS Club of XAUAT
       </p>
-      <div class="flex justify-center space-x-4">
-        <div class="flex items-center space-x-2">
+      <div class="flex justify-center">
+        <div class="flex items-center space-x-2 md:pl-2 desktop-phone-flex">
           <a class="a" href="https://cn.xauat.edu.cn/" target="_blank">
             西安建筑科技大学
           </a>
@@ -14,16 +14,16 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
           </svg>
         </div>
-        <div class="flex items-center space-x-2">
+        <div class="flex items-center md:space-x-2 md:pl-2 text-center">
           <a class="a" href="https://beian.miit.gov.cn/" target="_blank">
             备案号 陕ICP备2024031872号
           </a>
-          <svg class="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          <svg class="w-4 h-4 text-gray-500 desktop-phone" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
           </svg>
         </div>
-        <div class="flex items-center space-x-2">
+        <div class="flex items-center space-x-2 md:pl-2 desktop-phone-flex">
           <a class="a" href="https://gitee.com/XAUATiOSClub" target="_blank">
             我们的Gitee组织
           </a>
